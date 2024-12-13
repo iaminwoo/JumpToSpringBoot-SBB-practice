b7a6fff28e9e24f2f161e3e40240abf8ec6fc4b3
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-13 14:07:41 +0900

    2-06 & 2-07 ready

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,44 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.Question.Question;
+import com.mysite.sbb.Question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa1() {
+		Question question1 = new Question();
+		question1.setSubject("sbb가 무엇인가요?");
+		question1.setContent("sbb에 대해 알고 싶습니다.");
+		question1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(question1);
+
+		Question question2 = new Question();
+		question2.setSubject("스프링부트 모델 질문입니다.");
+		question2.setContent("id는 자동으로 생성되나요?");
+		question2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(question2);
+	}
+
+	@Test
+	void testJpa2() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question question = all.get(0);
+		assertEquals("sbb가 무엇인가요?", question.getSubject());
 	}
 
 }
