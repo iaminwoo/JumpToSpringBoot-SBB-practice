87b3a629e6f4b2cf225854712040220bf91dd44d
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-17 10:48:17 +0900

    p 2-12

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+
+    private final AnswerService answerService;
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer questionId,
+                               @RequestParam("content") String content) {
+        Question question = this.questionService.getQuestion(questionId);
+        this.answerService.createAnswer(content,question);
+        return String.format("redirect:/question/detail/%s", questionId);
+    }
+}
