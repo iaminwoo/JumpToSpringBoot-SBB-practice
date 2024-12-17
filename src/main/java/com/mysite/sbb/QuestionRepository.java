d9ec7c908a5cc8c8df5a5262227a5cbfada6ed67
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-17 09:49:47 +0900

    p 2-5

---
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String s);
+
+    Question findBySubjectAndContent(String s, String s1);
+
+    List<Question> findBySubjectLike(String s);
+}
