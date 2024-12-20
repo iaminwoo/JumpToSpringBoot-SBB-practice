ea3abc143930d8752d9cba4fc6e63843948a4765
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-20 10:15:16 +0900

    p2 2-04

---
+++ src/main/java/com/mysite/sbb/global/entity/PostBaseEntity.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class PostBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdDate;
+}
